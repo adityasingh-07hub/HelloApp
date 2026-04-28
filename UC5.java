284d37b8bf651cec6918db9255f179e51fd5f20b
adityasingh-07hub <adityaaingh2007@gmail.com>
2026-04-28 14:51:57 +0530

    Upload UC5

---
+++ UC5.java
@@ -0,0 +1,23 @@
+public class UC5 {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        System.out.println("Hello, " + nameBuilder + "!");
+    }
+}
